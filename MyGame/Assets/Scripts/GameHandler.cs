97fab3eafe3723f3404464073398e2b81a12b29f
Gwen Bradforth <gbradforth20students.polytechnic.org>
2020-02-09 23:07:16 -0800

    Added Unity files

---
+++ MyGame/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+     private GameObject Player;
+     public string myName;
+     public int end = -5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Player, I am" + myName + "!");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 p = transform.position;
+        Vector3 scaleChange = new Vector3(-0.01f, -0.01f, -0.01f);
+        if (!(p.y == end))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime);
+        }
+        if (p.y == end)
+        {
+            transform.localScale+=scaleChange;
+        }
+        Debug.Log(transform.position);    
+    }
+}
